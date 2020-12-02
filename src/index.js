500a260ba183ee356293f6df7e78bf5fcea30f93
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-02 12:03:27 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -49,10 +49,10 @@ function makePanel({ title, content } /* what data does the panel need? */) {
   const panel = document.createElement("div");
   const panelBar = document.createElement("div");
   const panelContent = document.createElement("div");
-  const panelTitle = document.createElement("div");
-  const panelButtons = document.createElement("button");
-  const openButton = document.createElement("div");
-  const closeButton = document.createElement("div");
+  const panelTitle = document.createElement("h3");
+  const panelButtons = document.createElement("div");
+  const openButton = document.createElement("button");
+  const closeButton = document.createElement("button");
 
   // TASK 6- Setup the structure of our elements
   /*
