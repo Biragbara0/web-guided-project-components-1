94fe59419f88438353f54f31ab740bb9bed6a5c0
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-02 13:01:31 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -112,9 +112,7 @@ accordion.appendChild(test);
 //  creating panels for each content and title and append them to the DOM.
 //  We can do this with a single forEach, or with a map and a forEach.
 
-panelData.forEach(panelObj => {
-  
-})
+const panelE
 
 // [STRETCH] Comment out the links inside the nav and
 // write a linkMaker that takes { href, className, text }
