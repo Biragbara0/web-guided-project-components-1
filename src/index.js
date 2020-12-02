064ce5b49c0472ee77ba1ce54f695e74a1a432af
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-02 11:19:59 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -17,6 +17,8 @@ function makeImage({ imageURL }) {
   return imgElement;
 }
 
+imageData.forEach(im)
+
 // TASK 1- Import the data we need to "hydrate" our component.
 //  On the one hand, the default export from data/panelData.js
 //  On the other hand, the default export from data/constants.js
